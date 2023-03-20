cf044ef5c46dbf853ea6e7d7ed39a685cc3e95d2
birdcountindia <skimmer@birdcount.in>
2023-03-20 09:35:28 +0530

    fixing name issues in Madhu's states and districts + different names for geometries

--- scripts/create_maps_sf.R
+++ scripts/create_maps_sf.R
@@ -24,6 +24,8 @@ grid_sizes_deg <- grid_sizes_km*1000/111111
 
 # reading maps ------------------------------------------------------------
 
+sf_use_s2(FALSE)
+
 india_sf <- st_read(dsn = country_path, layer = country_file) %>% mutate(DISTRICT = NULL) %>% 
   st_set_crs("OGC:CRS84")
 
@@ -37,11 +39,14 @@ states_sf <- st_read(dsn = states_path, layer = states_file) %>%
   mutate(STATE.NAME = case_when(STATE.NAME == "Dadra and Nagar Have" ~ "Dadra and Nagar Haveli",
                                 STATE.NAME == "Andaman and Nicobar" ~ "Andaman and Nicobar Islands",
                                 TRUE ~ STATE.NAME)) %>% 
-  st_set_crs("OGC:CRS84")
+  st_set_crs("OGC:CRS84") %>% 
+  rename(STATE.GEOM = geometry)
 
 dists_sf <- st_read(dsn = dists_path, layer = dists_file) %>% 
   dplyr::select(dtname, stname) %>% 
-  rename(DISTRICT.NAME = dtname, STATE.NAME = stname) %>% 
+  rename(DISTRICT.NAME = dtname, 
+         STATE.NAME = stname,
+         DISTRICT.GEOM = geometry) %>% 
   # some districts have two different rows (two different polygons) so need to combine
   # them into one polygon
   group_by(STATE.NAME, DISTRICT.NAME) %>% 
@@ -49,11 +54,26 @@ dists_sf <- st_read(dsn = dists_path, layer = dists_file) %>%
   ungroup() %>% 
   mutate(STATE.NAME = str_to_title(STATE.NAME)) %>% 
   # replacing ampersand with "and"
-  mutate(STATE.NAME = str_replace(STATE.NAME, "&", "and")) %>% 
+  mutate(STATE.NAME = str_replace(STATE.NAME, "&", "and"),
+         DISTRICT.NAME = str_replace(DISTRICT.NAME, "&", "and")) %>% 
   # corrections for Madhu's SPDF values
   mutate(STATE.NAME = case_when(STATE.NAME == "Dadra and Nagar Have" ~ "Dadra and Nagar Haveli",
                                 STATE.NAME == "Andaman and Nicobar" ~ "Andaman and Nicobar Islands",
-                                TRUE ~ STATE.NAME)) %>% 
+                                TRUE ~ STATE.NAME),
+         DISTRICT.NAME = case_when(DISTRICT.NAME == "Sivaganga" ~ "Sivagangai",
+                                   DISTRICT.NAME == "Thoothukkudi" ~ "Thoothukudi",
+                                   DISTRICT.NAME == "Raigarh" ~ "Raigad",
+                                   DISTRICT.NAME == "Nicobars" ~ "Nicobar",
+                                   DISTRICT.NAME == "Darjiling" ~ "Darjeeling",
+                                   DISTRICT.NAME == "Ahmadabad" ~ "Ahmedabad",
+                                   DISTRICT.NAME == "West District" ~ "West Sikkim",
+                                   DISTRICT.NAME == "Jagatsinghapur" ~ "Jagatsinghpur",
+                                   DISTRICT.NAME == "Mahrajganj" ~ "Maharajganj",
+                                   str_detect(DISTRICT.NAME, "North Twenty Four") ~ "North 24 Parganas",
+                                   str_detect(DISTRICT.NAME, "South Twenty Four") ~ "South 24 Parganas",
+                                   str_detect(DISTRICT.NAME, "Sri Potti Sriramulu") ~ "	
+Sri Potti Sriramulu Nellore",
+                                   TRUE ~ DISTRICT.NAME)) %>% 
   st_set_crs("OGC:CRS84")
 # https://gis.stackexchange.com/questions/421651/merging-two-multipolygon-shapefiles-and-removing-one-of-overlapping-polygons-usi
 
@@ -69,7 +89,7 @@ n <- (c(diff(st_bbox(india_sf)[c(1, 3)]), diff(st_bbox(india_sf)[c(2, 4)]))/cs) 
 g1_sf <- india_sf %>% 
   st_make_grid(cellsize = cs, n = n) %>% 
   st_as_sf() %>% 
-  rename(geometry = x) %>% 
+  rename(GEOM.G1 = x) %>% 
   # cell IDs
   rownames_to_column("GRID.G1")
 
@@ -88,7 +108,7 @@ n <- (c(diff(st_bbox(india_sf)[c(1, 3)]), diff(st_bbox(india_sf)[c(2, 4)]))/cs) 
 g2_sf <- india_sf %>% 
   st_make_grid(cellsize = cs, n = n) %>% 
   st_as_sf() %>% 
-  rename(geometry = x) %>% 
+  rename(GEOM.G2 = x) %>% 
   # cell IDs
   rownames_to_column("GRID.G2")
 
@@ -104,7 +124,7 @@ n <- (c(diff(st_bbox(india_sf)[c(1, 3)]), diff(st_bbox(india_sf)[c(2, 4)]))/cs) 
 g3_sf <- india_sf %>% 
   st_make_grid(cellsize = cs, n = n) %>% 
   st_as_sf() %>% 
-  rename(geometry = x) %>% 
+  rename(GEOM.G3 = x) %>% 
   # cell IDs
   rownames_to_column("GRID.G3")
 
@@ -120,7 +140,7 @@ n <- (c(diff(st_bbox(india_sf)[c(1, 3)]), diff(st_bbox(india_sf)[c(2, 4)]))/cs) 
 g4_sf <- india_sf %>% 
   st_make_grid(cellsize = cs, n = n) %>% 
   st_as_sf() %>% 
-  rename(geometry = x) %>% 
+  rename(GEOM.G4 = x) %>% 
   # cell IDs
   rownames_to_column("GRID.G4")
 
@@ -134,25 +154,23 @@ rm(cs, n)
 
 # intersecting grids with admin boundaries and calculating areas, totcells -----------
 
-sf_use_s2(FALSE)
-
 # already in sq.m because switched off S2 so using planar
 india_sf <- india_sf %>% mutate(AREA = units::set_units(round(st_area(geometry)), "km2"))
-states_sf <- states_sf %>% mutate(AREA = units::set_units(round(st_area(geometry)), "km2"))
-dists_sf <- dists_sf %>% mutate(AREA = units::set_units(round(st_area(geometry)), "km2"))
+states_sf <- states_sf %>% mutate(AREA = units::set_units(round(st_area(STATE.GEOM)), "km2"))
+dists_sf <- dists_sf %>% mutate(AREA = units::set_units(round(st_area(DISTRICT.GEOM)), "km2"))
 
 
 g1_in_sf <- st_intersection(g1_sf, india_sf) %>% 
-  mutate(AREA.G1 = units::set_units(round(st_area(geometry)), "km2"),
+  mutate(AREA.G1 = units::set_units(round(st_area(GEOM.G1)), "km2"),
          TOT.G1 = n_distinct(GRID.G1))
 g2_in_sf <- st_intersection(g2_sf, india_sf) %>% 
-  mutate(AREA.G2 = units::set_units(round(st_area(geometry)), "km2"),
+  mutate(AREA.G2 = units::set_units(round(st_area(GEOM.G2)), "km2"),
          TOT.G2 = n_distinct(GRID.G2))
 g3_in_sf <- st_intersection(g3_sf, india_sf) %>% 
-  mutate(AREA.G3 = units::set_units(round(st_area(geometry)), "km2"),
+  mutate(AREA.G3 = units::set_units(round(st_area(GEOM.G3)), "km2"),
          TOT.G3 = n_distinct(GRID.G3))
 g4_in_sf <- st_intersection(g4_sf, india_sf) %>% 
-  mutate(AREA.G4 = units::set_units(round(st_area(geometry)), "km2"),
+  mutate(AREA.G4 = units::set_units(round(st_area(GEOM.G4)), "km2"),
          TOT.G4 = n_distinct(GRID.G4))
 
 
