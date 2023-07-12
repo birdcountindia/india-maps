c354837cf5978035363ca098f24d80ed56a7a9c9
birdcountindia <skimmer@birdcount.in>
2023-07-12 17:54:00 +0530

    fixing admin unit issues with updated shapefiles

--- scripts/create_BT_NP_sf.R
+++ scripts/create_BT_NP_sf.R
@@ -1,45 +1,59 @@
-library(geodata)
 library(sf)
 library(tidyverse)
+library(glue)
 
-
-bt_sf <- geodata::gadm(country = "BTN", path = "outputs/temp_outputs/", level = 0) %>% 
-  st_as_sf() %>% 
-  dplyr::select(COUNTRY, geometry) %>% 
+# BHUTAN
+# (probable) source: https://data.humdata.org/dataset/cod-ab-btn?
+# Country > dzongkhag district > gewog
+bt_sf <- st_read(dsn = "data/bt_2020", layer = "bt_2020_adm0") %>% 
+  dplyr::select(ADM0_EN, geometry) %>% 
+  rename(COUNTRY = ADM0_EN) %>% 
   st_set_crs("OGC:CRS84")
 
-np_sf <- geodata::gadm(country = "NPL", path = "outputs/temp_outputs/", level = 0) %>% 
-  st_as_sf() %>% 
-  dplyr::select(COUNTRY, geometry) %>% 
+bt_states_sf <- st_read(dsn = "data/bt_2020", layer = "bt_2020_adm1") %>% 
+  dplyr::select(ADM1_EN, ADM0_EN, geometry) %>% 
+  rename(STATE.NAME = ADM1_EN,
+         COUNTRY = ADM0_EN,
+         STATE.GEOM = geometry) %>% 
   st_set_crs("OGC:CRS84")
 
-bt_states_sf <- geodata::gadm(country = "BTN", path = "outputs/temp_outputs/", level = 1) %>% 
-  st_as_sf() %>% 
-  rename(STATE.NAME = NAME_1) %>% 
-  dplyr::select(COUNTRY, STATE.NAME, geometry) %>% 
-  st_set_crs("OGC:CRS84") %>% 
-  rename(STATE.GEOM = geometry)
+bt_dists_sf <- st_read(dsn = "data/bt_2020", layer = "bt_2020_adm2") %>% 
+  dplyr::select(ADM2_EN, ADM0_EN, geometry) %>% 
+  rename(DISTRICT.NAME = ADM2_EN,
+         COUNTRY = ADM0_EN,
+         DISTRICT.GEOM = geometry) %>% 
+  st_set_crs("OGC:CRS84")
 
-bt_dists_sf <- geodata::gadm(country = "BTN", path = "outputs/temp_outputs/", level = 2) %>% 
-  st_as_sf() %>% 
-  rename(DISTRICT.NAME = NAME_2) %>% 
-  dplyr::select(COUNTRY, DISTRICT.NAME, geometry) %>% 
-  st_set_crs("OGC:CRS84") %>% 
-  rename(DISTRICT.GEOM = geometry)
 
-np_states_sf <- geodata::gadm(country = "NPL", path = "outputs/temp_outputs/", level = 1) %>% 
-  st_as_sf() %>% 
-  rename(STATE.NAME = NAME_1) %>% 
-  dplyr::select(COUNTRY, STATE.NAME, geometry) %>% 
-  st_set_crs("OGC:CRS84") %>% 
-  rename(STATE.GEOM = geometry)
+# NEPAL
+# source: https://opendatanepal.com/dataset/new-political-and-administrative-boundaries-shapefile-of-nepal
+# Country > province > district
+np_sf0 <- st_read(dsn = "data/np_2021", layer = "np_2021")
 
-np_dists_sf <- geodata::gadm(country = "NPL", path = "outputs/temp_outputs/", level = 2) %>% 
-  st_as_sf() %>% 
-  rename(DISTRICT.NAME = NAME_2) %>% 
-  dplyr::select(COUNTRY, DISTRICT.NAME, geometry) %>% 
-  st_set_crs("OGC:CRS84") %>% 
-  rename(DISTRICT.GEOM = geometry)
+np_sf <- np_sf0 %>% 
+  mutate(COUNTRY = "Nepal") %>% 
+  group_by(COUNTRY) %>% 
+  dplyr::summarise() %>% 
+  st_transform("OGC:CRS84")
+
+np_states_sf <- np_sf0 %>% 
+  mutate(COUNTRY = "Nepal") %>% 
+  group_by(COUNTRY, Province) %>% 
+  dplyr::summarise() %>% 
+  rename(STATE.NAME = Province,
+         STATE.GEOM = geometry) %>% 
+  st_transform("OGC:CRS84") %>% 
+  # adding "Province" to names
+  mutate(STATE.NAME = case_when(STATE.NAME %in% c(1, 2, 5) ~ glue("Province {STATE.NAME}"),
+                                TRUE ~ STATE.NAME))
+
+np_dists_sf <- np_sf0 %>% 
+  mutate(COUNTRY = "Nepal") %>% 
+  group_by(COUNTRY, DISTRICT) %>% 
+  dplyr::summarise() %>% 
+  rename(DISTRICT.NAME = DISTRICT,
+         DISTRICT.GEOM = geometry) %>% 
+  st_transform("OGC:CRS84")
 
 
 # saving maps
